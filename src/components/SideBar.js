cd0f1f2205b6f408e54c06c11b69086a9d7c8538
soledad <sol.rodriguez001@gmail.com>
2022-01-07 10:56:49 -0300

    enlazando

---
+++ src/components/SideBar.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import image from '../assets/images/logo-DH.png';
+import {Link, Route, Switch} from 'react-router-dom';
+
+import ContentWrapper from './ContentWrapper';
+import GenresInDb from './GenresInDb';
+import LastMovieInDb from './LastMovieInDb';
+import ContentRowMovies from './ContentRowMovies';
+import Error404 from './Error404';
+
+function SideBar(){
+    return(
+        <React.Fragment>
+            {/*<!-- Sidebar -->*/}
+            <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                <div className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                    <div className="sidebar-brand-icon">
+                        <img className="w-100" src={image} alt="Digital House"/>
+                    </div>
+                </div>
+
+                <hr className="sidebar-divider my-0"/>
+
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard - DH movies</span>
+                    </Link>
+                </li>
+
+                <hr className="sidebar-divider"/>
+
+                <div className="sidebar-heading">Actions</div>
+
+                <li className="nav-item">
+                    <Link className="nav-link" to="/GenresInDb">
+                        <i className="fas fa-fw fa-folder"></i>
+                        <span>Generos en BD</span>
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link className="nav-link" to="/LastMovieInDb">
+                        <i className="fas fa-fw fa-chart-area"></i>
+                        <span>Última película en BD</span>
+                    </Link>
+                </li>
+
+                <li className="nav-item nav-link">
+                    <Link className="nav-link" to="/ContentRowMovies">
+                        <i className="fas fa-fw fa-table"></i>
+                        <span>Tarjetas</span>
+                    </Link>
+                </li>
+                <hr className="sidebar-divider d-none d-md-block"/>
+            </ul>
+            {/*<!-- End of Sidebar -->*/}
+
+            <Switch>
+                <Route exact path="/">
+                    <ContentWrapper />
+                </Route>
+                <Route path="/GenresInDb">
+                    <GenresInDb />
+                </Route>
+                <Route path="/LastMovieInDb">
+                    <LastMovieInDb />
+                </Route>
+                <Route path="/ContentRowMovies">
+                    <ContentRowMovies />
+                </Route>
+                <Route component={Error404} />
+            </Switch>
+            
+        </React.Fragment>
+    )
+}
+export default SideBar;
